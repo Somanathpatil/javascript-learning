4d2d30eb4b707085d2f7036d245030bea67a7a53
Somanathpatil <patilsomanath292@gmail.com>
2025-07-24 22:36:24 +0530

    this is begining

---
+++ basics/dataType.js
@@ -0,0 +1,19 @@
+// "use strict" treat all code as a new version
+
+// alert(3+3)  we are using node js not browser
+
+// code readability is importent
+
+// number = 2 power of 53
+// string
+// boolean
+// bigint
+// null = stand alone value
+// undefined
+// symbol = unique
+
+// object
+
+console.log(typeof undefined);  //type is undefined
+console.log(typeof null); //type is object
+
---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 1223434
+let accountEmail = "patilsomanath292@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
