62257d945168aabd19755ba173f1390340f2e7ad
Somanathpatil <patilsomanath292@gmail.com>
2025-07-26 11:56:54 +0530

    hfuguig

---
+++ basics/numbers-maths.js
@@ -0,0 +1,18 @@
+const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+// ++++++++++++++Math+++++++++++++++++++
+
+console.log(Math);
+console .log(Math.abs(-4));
+console.log(Math.round(5.6));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(1,3,5,4,7));
+console.log(Math.max(1,23,4,56,6,5,));
