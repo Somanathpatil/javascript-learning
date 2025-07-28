082867763f9ef3bf7202564d3107671ab59b1c15
Somanathpatil <patilsomanath292@gmail.com>
2025-07-28 21:08:39 +0530

    ther yyt

--- basics/array.js
+++ basics/array.js
@@ -1,3 +1,17 @@
 const myArry = [1,2,3,4,5];
 console.log(myArry)
 console.log(myArry[0])
+
+myArry.push(6);
+console.log(myArry);
+
+myArry.pop()
+console.log(myArry);
+
+console.log(myArry.includes(4))
+console.log(myArry.indexOf(4))
+
+const newArry = myArry.join()
+
+console.log(newArry)
+console.log(typeof newArry)
